4c69b1e943cefedf308f41ded807171995e02d18
salauddin <salauddin@hasan.com>
2026-03-02 20:46:18 +0600

    Implement fetch API request on Load button click

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+const dataData = async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Fetch error:", error);
+  }
+};
